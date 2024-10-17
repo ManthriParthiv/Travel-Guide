1c77df9ac953860d161d087d3803cfe939f3d205
Komaramsai <saikomaram7@gmail.com>
2024-10-17 11:42:44 +0530

    adding style and content to homepage about us page and contact us page

--- mernapp/src/components/Weather.js
+++ mernapp/src/components/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../styles/weather.css'
 
 const WeatherReport = () => {
   const [weatherData, setWeatherData] = useState(null);
