76cb89fe28aebd8499a78ea79ca9e37d5194d6ba
Komaramsai <saikomaram1@gmail.com>
2024-10-24 09:41:13 +0530

    change the names of the hotels in travel guide

--- mernapp/src/screens/Travelbooking.js
+++ mernapp/src/screens/Travelbooking.js
@@ -257,23 +257,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Standard Room",
-                        price: "$150 per night",
-                        highestPrice: "$180",
-                        lowestPrice: "$130",
+                        price: "1500 per night",
+                        highestPrice: "1800",
+                        lowestPrice: "1300",
                         img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2c/b0/b9/d0/cheap-hotels.jpg?w=1200&amp;h=-1&amp;s=1"
                     },
                     {
                         type: "Deluxe Room",
-                        price: "$200 per night",
-                        highestPrice: "$220",
-                        lowestPrice: "$180",
+                        price: "2000 per night",
+                        highestPrice: "2200",
+                        lowestPrice: "1800",
                         img: "https://r2imghtlak.mmtcdn.com/r2-mmt-htl-image/room-imgs/201804031804379537-3703456-0a49deaa603711e9a96a0242ac110002.jpg"
                     },
                     {
                         type: "Suite Room",
-                        price: "$300 per night",
-                        highestPrice: "$350",
-                        lowestPrice: "$280",
+                        price: "3000 per night",
+                        highestPrice: "3500",
+                        lowestPrice: "2800",
                         img: "https://www.itchotels.com/content/dam/itchotels/in/umbrella/itc/hotels/itckohenur-hyderabad/images/overview/headmast-desktop/towers.png"
                     }
                 ],
@@ -289,23 +289,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Standard Room",
-                        price: "$150 per night",
-                        highestPrice: "$170",
-                        lowestPrice: "$130",
+                        price: "1500 per night",
+                        highestPrice: "1700",
+                        lowestPrice: "1300",
                         img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/539960114.jpg?k=ef64a964109f3fb539791936503182d84da21959e26cb18c95a83a1e38c8f534&amp;o=&amp;hp=1"
                     },
                     {
                         type: "Executive Room",
-                        price: "$250 per night",
-                        highestPrice: "$270",
-                        lowestPrice: "$230",
+                        price: "2500 per night",
+                        highestPrice: "2700",
+                        lowestPrice: "2300",
                         img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/522909731.jpg?k=d62fb92afb03fa18142690c714b2e995462f7dfe9f704c6397a186b8127242c6&amp;o=&amp;hp=1"
                     },
                     {
                         type: "Luxury Suite",
-                        price: "$400 per night",
-                        highestPrice: "$450",
-                        lowestPrice: "$380",
+                        price: "4000 per night",
+                        highestPrice: "4500",
+                        lowestPrice: "3800",
                         img: "https://cf.bstatic.com/xdata/images/hotel/max200/522909720.jpg?k=a2c196d19e76ea41eb78e3207cb1ef1403ce776d6e49271cf3272e8012c2ead0&amp;o=&amp;hp=1"
                     }
                 ],
@@ -321,23 +321,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Luxury Room",
-                        price: "$350 per night",
-                        highestPrice: "$400",
-                        lowestPrice: "$300",
+                        price: "3500 per night",
+                        highestPrice: "4000",
+                        lowestPrice: "3000",
                         img:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/461630725.jpg?k=8755461b6f4eeeb3875a41aaeca7fd2bb1e2eac5e355e7596fff002610fdf069&amp;o=&amp;hp=1"
                     },
                     {
                         type: "Presidential Suite",
-                        price: "$800 per night",
-                        highestPrice: "$900",
-                        lowestPrice: "$750",
+                        price: "8000 per night",
+                        highestPrice: "9000",
+                        lowestPrice: "7500",
                         img: "https://www.itchotels.com/content/dam/itchotels/in/umbrella/itc/hotels/itcmaratha-mumbai/images/overview-landing-page/headmast/desktop/shivaji-presidential-suite.png"
                     },
                     {
                         type: "Family Room",
-                        price: "$400 per night",
-                        highestPrice: "$450",
-                        lowestPrice: "$380",
+                        price: "4000 per night",
+                        highestPrice: "4500",
+                        lowestPrice: "3800",
                         img:"https://pix10.agoda.net/hotelImages/41037428/-1/b1044eb247cbbba4895b62e51782f021.jpg?ce=0&amp;s=702x392"
                     }
                 ],
@@ -353,23 +353,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Suite Room",
-                        price: "$500 per night",
-                        highestPrice: "$550",
-                        lowestPrice: "$480",
+                        price: "5000 per night",
+                        highestPrice: "5500",
+                        lowestPrice: "4800",
                         img:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/550559664.jpg?k=2b872efe6a4cca553b22ba457cd37e175cfb62617b26849715d45619de14dc40&amp;o=&amp;hp=1"
                     },
                     {
                         type: "Business Room",
-                        price: "$300 per night",
-                        highestPrice: "$350",
-                        lowestPrice: "$280",
+                        price: "3000 per night",
+                        highestPrice: "3500",
+                        lowestPrice: "2800",
                         img:"https://images.oyoroomscdn.com/uploads/hotel_image/2600/small/ab0565e4d9a68c6b.jpg"
                     },
                     {
                         type: "Standard Room",
-                        price: "$200 per night",
-                        highestPrice: "$220",
-                        lowestPrice: "$180",
+                        price: "2000 per night",
+                        highestPrice: "2200",
+                        lowestPrice: "1800",
                         img:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/331797608.jpg?k=358e3fe8ae7ff684f46e26b0c65e14da3116bc5d1c69a10a8a4dedfa0366e994&amp;o=&amp;hp=1"
                     }
                 ],
@@ -385,23 +385,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Business Room",
-                        price: "$220 per night",
-                        highestPrice: "$240",
-                        lowestPrice: "$200",
+                        price: "2200 per night",
+                        highestPrice: "2400",
+                        lowestPrice: "2000",
                         img:"https://www.itchotels.com/content/dam/itchotels/in/umbrella/itc/hotels/itcroyalbengal-kolkata/images/overview-landing-page/headmast/desktop/itc-one-sitting-room.jpg"
                     },
                     {
                         type: "Deluxe Room",
-                        price: "$250 per night",
-                        highestPrice: "$270",
-                        lowestPrice: "$230",
+                        price: "2500 per night",
+                        highestPrice: "2700",
+                        lowestPrice: "2300",
                         img:"https://www.momondo.in/himg/ee/17/4a/leonardo-1292297-2_-_Premium_room-premium_suite_sleeping_room_alt_O-759937.jpg"
                     },
                     {
                         type: "Suite Room",
-                        price: "$350 per night",
-                        highestPrice: "$400",
-                        lowestPrice: "$320",
+                        price: "3500 per night",
+                        highestPrice: "4000",
+                        lowestPrice: "3200",
                         img:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/27150803.jpg?k=95ce51f71f77ab0cd31a60fbdec22e3f2fda7141e4bea4dd9efe5595df565e0d&amp;o=&amp;hp=1"
                     }
                 ],
@@ -417,23 +417,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Standard Room",
-                        price: "$180 per night",
-                        highestPrice: "$200",
-                        lowestPrice: "$160",
+                        price: "1800 per night",
+                        highestPrice: "2000",
+                        lowestPrice: "1600",
                         img:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/497787661.jpg?k=58bff7b48e28c244b09ff4c20b96b1634efdab6d6734148a370e43214baa061b&amp;o=&amp;hp=1"
                     },
                     {
                         type: "Executive Room",
-                        price: "$250 per night",
-                        highestPrice: "$280",
-                        lowestPrice: "$230",
+                        price: "2500 per night",
+                        highestPrice: "2800",
+                        lowestPrice: "2300",
                         img:"https://cdn.sanity.io/images/ocl5w36p/production/36826de7385066979f89fb9643b6c17fe194459d-1281x1760.jpg?w=480&amp;fm=webp&amp;dpr=2"
                     },
                     {
                         type: "Luxury Suite",
-                        price: "$400 per night",
-                        highestPrice: "$450",
-                        lowestPrice: "$380",
+                        price: "4000 per night",
+                        highestPrice: "4500",
+                        lowestPrice: "3800",
                         img:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/344764297.jpg?k=074a0878a6ec61ccbbe4b845871ea77ad7cca0bb89e6fc0f3fffff9e76de4ee5&amp;o=&amp;hp=1"
                     }
                 ],
@@ -449,23 +449,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Standard Room",
-                        price: "$130 per night",
-                        highestPrice: "$150",
-                        lowestPrice: "$120",
+                        price: "1300 per night",
+                        highestPrice: "1500",
+                        lowestPrice: "1200",
                         img:"https://www.themayurahotels.com/newest-img/rooms/suite_room/Suite%20room3_DSC4655.jpg"
                     },
                     {
                         type: "Deluxe Room",
-                        price: "$200 per night",
-                        highestPrice: "$220",
-                        lowestPrice: "$180",
+                        price: "2000 per night",
+                        highestPrice: "2200",
+                        lowestPrice: "1800",
                         img:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/270058077.jpg?k=1ea3da56cba31b23bcd329fcec1b721676fad88cfc41ddac769980677ae3554d&amp;o=&amp;hp=1"
                     },
                     {
                         type: "Family Room",
-                        price: "$250 per night",
-                        highestPrice: "$280",
-                        lowestPrice: "$230",
+                        price: "2500 per night",
+                        highestPrice: "2800",
+                        lowestPrice: "2300",
                         img: "https://hotel-babylon-inn-raipur.booked.net/data/Photos/OriginalPhoto/6836/683638/683638884/Hotel-Babylon-Inn-Raipur-Exterior.JPEG"
                     }
                 ],
@@ -481,23 +481,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Ocean View Room",
-                        price: "$220 per night",
-                        highestPrice: "$250",
-                        lowestPrice: "$200",
+                        price: "2200 per night",
+                        highestPrice: "2500",
+                        lowestPrice: "2000",
                         img: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/603208166.jpg?k=07715df961e5221d27e66511848a32e70c20309791144a5a17ae1ec5e9eef24b&amp;o=&amp;hp=1"
                     },
                     {
                         type: "Deluxe Room",
-                        price: "$250 per night",
-                        highestPrice: "$280",
-                        lowestPrice: "$230",
+                        price: "2500 per night",
+                        highestPrice: "2800",
+                        lowestPrice: "2300",
                         img:"https://www.vivantahotels.com/content/dam/vivanta/hotels/VBT-Panaji/images/Retreat/PictureA.jpg"
                     },
                     {
                         type: "Suite Room",
-                        price: "$350 per night",
-                        highestPrice: "$400",
-                        lowestPrice: "$320",
+                        price: "3500 per night",
+                        highestPrice: "4000",
+                        lowestPrice: "3200",
                         img: "https://www.momondo.in/rimg/himg/bd/f4/13/leonardo-56605-153242771-105919.jpg?width=968&amp;height=607&amp;crop=true"
                     }
                 ],
@@ -513,23 +513,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Standard Room",
-                        price: "$140 per night",
-                        highestPrice: "$160",
-                        lowestPrice: "$120",
+                        price: "1400 per night",
+                        highestPrice: "1600",
+                        lowestPrice: "1200",
                         img:"https://www.holidify.com/images/cmsuploads/compressed/232239351_20221125034908.jpg"
                     },
                     {
                         type: "Deluxe Room",
-                        price: "$180 per night",
-                        highestPrice: "$200",
-                        lowestPrice: "$160",
+                        price: "1800 per night",
+                        highestPrice: "2000",
+                        lowestPrice: "1600",
                         img: "https://res.cloudinary.com/simplotel/image/upload/w_5000,h_3333/x_0,y_260,w_5000,h_2812,r_0,c_crop,q_80,fl_progressive/w_900,f_auto,c_fit/hotel-meghdoot-bhopal/Meghdoot_Hotel_10-09-2023_Studio_New_Folder_001_tfinfi"
                     },
                     {
                         type: "Executive Room",
-                        price: "$220 per night",
-                        highestPrice: "$240",
-                        lowestPrice: "$200",
+                        price: "2200 per night",
+                        highestPrice: "2400",
+                        lowestPrice: "2000",
                         img:"https://www.treehousehideaway.com/images/thh-internal.jpg"
                     }
                 ],
@@ -545,23 +545,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Ocean View Room",
-                        price: "$230 per night",
-                        highestPrice: "$250",
-                        lowestPrice: "$210",
+                        price: "2300 per night",
+                        highestPrice: "2500",
+                        lowestPrice: "2100",
                         img:"https://www.ambicaseagreen.com/header/2a.jpg"
                     },
                     {
                         type: "Standard Room",
-                        price: "$160 per night",
-                        highestPrice: "$180",
-                        lowestPrice: "$140",
+                        price: "1600 per night",
+                        highestPrice: "1800",
+                        lowestPrice: "1400",
                         img:"https://www.theparkhotels.com/images/site-specific/visakhapatnam/rooms/luxury_rooms/luxury-01.jpg"
                     },
                     {
                         type: "Luxury Suite",
-                        price: "$350 per night",
-                        highestPrice: "$400",
-                        lowestPrice: "$320",
+                        price: "3500 per night",
+                        highestPrice: "4000",
+                        lowestPrice: "3200",
                         img:"https://imgcld.yatra.com/ytimages/image/upload/t_seo_Hotel_w_930_h_550_c_fill_g_auto_q_40_f_jpg/v1503385214/Hotel/Visakhapatnam/00000651/Premium_Room_wdKzRy.jpg"
                     }
                 ],
@@ -577,23 +577,23 @@ export default function Home() {
                 rooms: [
                     {
                         type: "Standard Room",
-                        price: "$150 per night",
-                        highestPrice: "$170",
-                        lowestPrice: "$130",
+                        price: "1500 per night",
+                        highestPrice: "1700",
+                        lowestPrice: "1300",
                         img:"https://images.trvl-media.com/lodging/2000000/1900000/1900000/1899945/1fb4ec7d.jpg?impolicy=fcrop&amp;w=1200&amp;h=800&amp;p=1&amp;q=medium"
                     },
                     {
                         type: "Deluxe Room",
-                        price: "$220 per night",
-                        highestPrice: "$240",
-                        lowestPrice: "$200",
+                        price: "2200 per night",
+                        highestPrice: "2400",
+                        lowestPrice: "2000",
                         img:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/63/d3/20/luxury-suit.jpg?w=700&amp;h=-1&amp;s=1"
                     },
                     {
                         type: "Suite Room",
-                        price: "$300 per night",
-                        highestPrice: "$350",
-                        lowestPrice: "$280",
+                        price: "3000 per night",
+                        highestPrice: "3500",
+                        lowestPrice: "2800",
                         img:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/450679631.jpg?k=5495e459c47bf24b4bc81d7330e5dd9086e25fb3758232d07650b0abc33d997c&amp;o=&amp;hp=1"
                     }
                 ],
@@ -630,7 +630,7 @@ export default function Home() {
                     type="text" 
                     value={countryInput} 
                     onChange={(e) => setCountryInput(e.target.value)} 
-                    placeholder="Enter Country Name" 
+                    placeholder="Enter City Name" 
                 />
                 <button onClick={handleSearch}>Search</button>
 
@@ -670,7 +670,7 @@ export default function Home() {
                         <button onClick={() => setSelectedCountry(null)}>Back to Search</button>
                     </div>
                 ) : (
-                    <div>Please enter a country to see available hotels.</div>
+                    <div>Please enter a city to see available hotels.</div>
                 )}
             </main>
             <Footer />
